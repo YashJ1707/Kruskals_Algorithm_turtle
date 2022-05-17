167df6a94cd8775a5b6188bad467f2fbf18b0ba7
YashJ1707 <78841896+YashJ1707@users.noreply.github.com>
2022-05-17 22:24:30 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,82 @@
+# Kruskal's algorithm in Python
+import turtle
+tu=turtle.Turtle()
+scale=50
+
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
+
+    def add_edge(self, u, v, w):
+        self.graph.append([u, v, w])
+
+    # Search function
+
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+
+    def apply_union(self, parent, rank, x, y):
+        xroot = self.find(parent, x)
+        yroot = self.find(parent, y)
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+        else:
+            parent[yroot] = xroot
+            rank[xroot] += 1
+
+    #  Applying Kruskal algorithm
+    def kruskal_algo(self):
+        result = []
+        i, e = 0, 0
+        self.graph = sorted(self.graph, key=lambda item: item[2])
+        parent = []
+        rank = []
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+        while e < self.V - 1:
+            u, v, w = self.graph[i]
+            i = i + 1
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+            if x != y:
+                e = e + 1
+                result.append([u, v, w])
+                self.apply_union(parent, rank, x, y)
+        tu.color("green")
+        tu.left(90)
+        for u, v, weight in result:
+          # tu.forward(-weight*20)
+          tu.penup()
+          tu.forward(weight*20/2)
+          tu.write(str(weight))
+          tu.forward(-weight*20/2)
+          tu.pendown() 
+          tu.forward(weight*20)
+          # turtle.write(weight, align="right", font=34)
+          tu.right(40)
+          print("%d - %d: %d" % (u, v, weight))
+
+
+g = Graph(6)
+g.add_edge(0, 1, 5)
+g.add_edge(0, 2, 4)
+g.add_edge(1, 2, 5)
+g.add_edge(1, 0, 2)
+g.add_edge(2, 0, 8)
+g.add_edge(2, 1, 4)
+g.add_edge(2, 3, 3)
+g.add_edge(2, 5, 7)
+g.add_edge(2, 4, 4)
+g.add_edge(3, 2, 3)
+g.add_edge(3, 4, 7)
+g.add_edge(4, 2, 6)
+g.add_edge(4, 3, 4)
+g.add_edge(5, 2, 8)
+g.add_edge(5, 4, 7)
+g.kruskal_algo()
